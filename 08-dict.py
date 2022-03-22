6ff0bcd8c0a6e8c072b9db8210c57c73d7932c81
ankush-bhawar <ankush.bhawar07@gmail.com>
2022-03-22 23:34:42 +0530

    dictionary in python

---
+++ 08-dict.py
@@ -0,0 +1,53 @@
+# Creating an empty dictionary
+d1 = dict()
+print(type(d1))
+
+d2 = {}
+print(type(d2))
+
+d3 = {"Ankush": 8830400295, "CodeCompete": "08830400295", 3: "Three"}
+print(d3)
+
+print(d3["Ankush"])
+print(d3.keys())
+print(d3.values())
+
+# accessing using key
+print(d3["Ankush"])
+
+# get() ( accessing using get method )
+print(d3.get("Ankushsd", "Not found"))
+
+# add()
+d3["John"] = 348763287234832487
+print(d3)
+
+# del
+del d3[3]
+print(d3)
+
+# update()
+oldDict = {
+"sjkdhksj": "sdmnbsm",
+"jsdhgfdsjf": "dnbvsvdsbdvsb"
+}
+d3.update(oldDict)
+print(d3)
+
+d3.pop("Ankush")
+print(d3)
+print(d3)
+
+# merge
+a1 = ["a", "b", "c"]
+a2 = [1,2,3]     
+a3 = dict(zip(a1,a2))
+print(a3)
+
+# clear()
+d3.clear()
+print(d3)
+
+# del function
+del d3
+# print(d3) ( it will through error, because we don't have d3 now )
